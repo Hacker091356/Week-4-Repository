33e2951f0a037f7379ff3c231b9af50da573c666
Hacker091356 <ninno2812@gmail.com>
2024-03-10 11:24:09 -0400

    DIGITS

---
+++ digits.java
@@ -0,0 +1,25 @@
+// Importing scanner
+import java.util.Scanner;
+public class digits {
+    public static void main(String[] args){
+        //Initialize scanner 
+        Scanner userInput = new Scanner(System.in);
+        //Ask for input
+        System.out.print("Enter a 3 digit number:");
+        int number = userInput.nextInt();
+        int onesPlace = number % 10;
+        int tensForCalculatingHundreds = (number - (onesPlace))%10;
+        int tensPlace = (number % 100 - onesPlace)/ 10;
+        int hundredsPlace = (number - onesPlace - tensForCalculatingHundreds)/100;
+        //Displaying results
+        System.out.println("The hundreds digit is: " + hundredsPlace);
+        System.out.println("The tens digit is: " + tensPlace);
+        System.out.println("The ones digit is: " + onesPlace);
+
+        
+
+
+
+    }
+    
+}
